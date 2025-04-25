b89ea4cd839fea50122b793df3d1c9c56bcc4570
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2025-04-25 18:58:08 -0500

    Se modifica el controller

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import med.voll.api.medico.DatosRegistroMedico;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class MedicoController {
 
     @PostMapping
-    public void registroMedico(@RequestBody String parametro) {
-        System.out.println("La ruta esta funcionando correctamente");
-        System.out.println(parametro);
+    public void registroMedico(@RequestBody DatosRegistroMedico datosRegistroMedico) {
+
+        System.out.println(datosRegistroMedico.direccion());
     }
 }
